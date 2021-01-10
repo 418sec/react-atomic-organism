6ba419dabc7ef1f7fdc9c491820563b7a7d27008
d3v53c <shitfi63@gmail.com>
2021-01-10 02:28:32 -0800

    prototype pollution fix

--- packages/lib/set-object-value/src/index.js
+++ packages/lib/set-object-value/src/index.js
@@ -1,6 +1,16 @@
+/**
+ * Returns true, if given key is included in the blacklisted
+ * keys.
+ * @param key key for check, string.
+ */
+function isPrototypePolluted(key) {
+  return ['__proto__', 'prototype', 'constructor'].includes(key);
+}
+
 const replaceValue = (obj, arrKey, val, isAppend) => {
   const last = arrKey.pop();
   arrKey.forEach((k) => {
+    if (isPrototypePolluted(k)) return;
     obj[k] = obj[k] || {};
     obj = obj[k];
   });
